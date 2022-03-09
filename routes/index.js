e461bc5c10d1822111477113836c7bf40b81e7d7
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2022-03-09 13:03:22 -0500

    some wildcard endpoints

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:colour', function(req, res, next) {
+    res.render('index', { title: 'Web4R', colour: req.params.colour });
 });
 
 module.exports = router;
